3867bcb721e509782678c79ad2160abd199dd6b3
LuckyTeemo <269188607@qq.com>
2018-03-06 19:03:12 +0800

    测试idea 提交代码

---
+++ src/main/java/com/shsxt/demo/UserController.java
@@ -0,0 +1,12 @@
+package com.shsxt.demo;
+
+public class UserController {
+
+	public boolean userLogin(String name,String pwd) {
+		return false;
+	}
+
+	public void print () {
+		System.out.println("=====");
+	}
+}
